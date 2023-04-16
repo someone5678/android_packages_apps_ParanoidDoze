ResurrectionRemix-Devices/device_oneplus_sdm845-common

sdm845-common: doze: Fix pickup status for raise to wake
Signed-off-by: AnierinB <anierinb@evolution-x.org>

doze/src/org/evolution/settings/doze/DozeSettingsFragment.java
@@ -149,8 +149,8 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
             Utils.enableAlwaysOn(getActivity(), (Boolean) newValue);
         }
 
-	if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(preference.getKey())) {
-	    Utils.setPickUp(getActivity(), (Boolean) newValue);
+        if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(preference.getKey())) {
+            Utils.setPickUp(findPreference(Utils.GESTURE_PICK_UP_KEY), (Boolean) newValue);
         }
 
         mHandler.post(() -> Utils.checkDozeService(getActivity()));

someone5678/android_packages_apps_ParanoidDoze

src/co/aospa/doze/DozeSettingsFragment.java
@@ -132,7 +132,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         mHandler.post(() -> DozeUtils.checkDozeService(getActivity()));
 
         if (DozeUtils.GESTURE_RAISE_TO_WAKE_KEY.equals(preference.getKey())) {
-            DozeUtils.setPickUp(getActivity(), (Boolean) newValue);
+            DozeUtils.setPickUp(findPreference(DozeUtils.GESTURE_PICK_UP_KEY), (Boolean) newValue);
         }
 
         return true;